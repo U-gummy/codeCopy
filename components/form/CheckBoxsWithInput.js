80d39147eeebc6afb2241d57ff34f6a297c5c69e
yougmi <zoz7353@gmail.com>
2022-04-28 11:49:10 +0900

    with chk box row style

--- components/form/CheckBoxsWithInput.js
+++ components/form/CheckBoxsWithInput.js
@@ -5,8 +5,19 @@ export default function CheckBoxsWithInput() {
         <label>with input</label>
         <input type="text" />
         <div className="chk-box">
-          <input type="checkbox" id="check7"></input>
-          <label htmlFor="check7">Default Checkbox</label>
+          <input type="checkbox" id="check11"></input>
+          <label htmlFor="check11">Default Checkbox</label>
+        </div>
+      </div>
+
+      <div className="input-box">
+        <label>with input</label>
+        <div className="with-chk-box-row">
+          <input type="text" />
+          <div className="chk-box">
+            <input type="checkbox" id="check12"></input>
+            <label htmlFor="check12">Default Checkbox</label>
+          </div>
         </div>
       </div>
     </>
